d7821deeb849a9e3bcfcb5d03b8924eb983019d5
Whelk Release <noreply@getwhelk.com>
2026-08-26 10:07:13 +0000

    Update whelk to nightly-20260826

--- Casks/whelk.rb
+++ Casks/whelk.rb
@@ -1,8 +1,8 @@
 cask "whelk" do
-  version "nightly-20260825"
-  sha256 "2a518697b1272bce2b22a1aa5f56c80f8c28658fcded8894ae38aa9439dc986c"
+  version "nightly-20260826"
+  sha256 "9e08f11f282c12f019836201b4cd48a538818301bba63e6535b8beb1e6fba6a1"
 
-  url "https://downloads.getwhelk.com/apple/nightly-20260825/Whelk-nightly-20260825.dmg"
+  url "https://downloads.getwhelk.com/apple/nightly-20260826/Whelk-nightly-20260826.dmg"
   name "Whelk"
   desc "Native client for models running on your own hardware"
   homepage "https://getwhelk.com"
