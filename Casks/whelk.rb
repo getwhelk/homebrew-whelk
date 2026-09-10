92460da80721641059ece82c9fd012a3062fe4c8
Whelk Release <noreply@getwhelk.com>
2026-09-10 19:59:13 +0000

    Update whelk to train-2026.9.0

--- Casks/whelk.rb
+++ Casks/whelk.rb
@@ -1,8 +1,8 @@
 cask "whelk" do
-  version "nightly-20260910"
-  sha256 "6c1b3313a8c5a8ab7dec22671325102cd9b338f625172463f286c3ca24384040"
+  version "train-2026.9.0"
+  sha256 "bcbdfa75336fb508850ec5cc10e240f087fe2b061a967a66749414402247aac1"
 
-  url "https://downloads.getwhelk.com/apple/nightly-20260910/Whelk-nightly-20260910.dmg"
+  url "https://downloads.getwhelk.com/apple/train-2026.9.0/Whelk-train-2026.9.0.dmg"
   name "Whelk"
   desc "Native client for models running on your own hardware"
   homepage "https://getwhelk.com"
